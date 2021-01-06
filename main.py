510234ca683289e993f3a5fd3b86e3bb8c41e7cf
刘子夕 <liuzixi@liuzixideMacBook-Pro.local>
2021-01-06 13:28:14 +0800

    update code

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import os
 
 
 if __name__ == '__main__':
-    wrapper = "/home/cici/major/DialTest/save_profile/nlu-model/snips_abst_train_model/wrapper_lstm_nlu_3_10.pkl"
-    test_data_path = "/home/cici/Desktop/snips/standard/test"
-    out_path = "/home/cici/major/DialTest/data/snips/out/out.txt"
+    wrapper = "/root/chatbot/DialTest/save_profile/nlu-model/snips_abst_train_model/wrapper_lstm_nlu_3_10.pkl"
+    test_data_path = "/root/chatbot/DialTest/data/snips/test"
+    out_path = "/root/chatbot/DialTest/data/snips/out/out.txt"
 
     BSCov_now, BTCov_now = 0, 0
-    seed_data_path = "/home/cici/major/DialTest/data/snips/seed"
+    seed_data_path = "/root/chatbot/DialTest/data/snips/seed"
     in_test_data = open(test_data_path, mode='r', encoding='utf-8')
     seed_data = open(seed_data_path, mode='a', encoding='utf-8')
 
     lines = in_test_data.readlines()
     for j, line in enumerate(lines):
-        states_path = "/home/cici/major/DialTest/data/snips/states/out_" + str(j) + ".npy"
-        tmp_seed_data_path = "/home/cici/major/DialTest/data/snips/seed_tmp"
+        states_path = "/root/chatbot/DialTest/data/snips/states/out_" + str(j) + ".npy"
+        tmp_seed_data_path = "/root/chatbot/DialTest/data/snips/seed_tmp"
         tmp_seed_data = open(tmp_seed_data_path, mode='w', encoding='utf-8')
         tmp_seed_data.writelines(lines[:j+1])
         tmp_seed_data.close()
